a06a7f1fe58ec19d08bd487ee5862335f4a3b1b2
Ruvick <rudikov.web@ya.ru>
2022-04-25 12:33:26 +0300

    Заменил инстаграмм на телеграмм

--- footer.php
+++ footer.php
@@ -8,8 +8,8 @@
 
 			<div class="footer__info">
 				<a href="<?php echo carbon_get_theme_option('as_vk'); ?>" class="social-link header__social-link footer__social-link-01"></a>
-				<a href="<?php echo carbon_get_theme_option('as_insta'); ?>" class="social-link header__social-link footer__social-link-02"></a>
-				<p class="footer__info-policy">Бизнеснавигатор 2022©</p>
+				<a href="<?php echo carbon_get_theme_option('as_telegr'); ?>" class="social-link header__social-link footer__social-link-02"></a>
+				<p class="footer__info-policy">Бизнеснавигатор 2022©</p> 
 			</div>
 
 		</div>
